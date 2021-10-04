806c12a544052602450f214ac14429a87a467827
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2021-10-04 13:44:53 +0700

    Praktikum - Flutter Chapter 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,136 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+  build(context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        home: Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red[800], title: Text('MyApp')),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 50,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Text(
+                  'BERITA HARI INI',
+                  textAlign: TextAlign.center,
+                )),
+                Expanded(
+                    child: Text(
+                  'PERTANDINGAN HARI INI',
+                  textAlign: TextAlign.center,
+                )),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(color: Colors.purple),
+                    left: BorderSide(color: Colors.purple),
+                    right: BorderSide(color: Colors.purple))),
+            child: Image.network(
+                'https://images.daznservices.com/di/library/GOAL/96/c2/diego-costa_1dq1b6p5g1d1w1fhzzirww8bie.jpg?t=-1517427818&quality=100.jpg'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    left: BorderSide(color: Colors.purple),
+                    right: BorderSide(color: Colors.purple))),
+            height: 40,
+            child: Center(
+              child: Text(
+                'Costa Mendekat Ke Palmiera',
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            width: 400,
+            height: 50,
+            alignment: Alignment(-0.9, 0.0),
+            decoration: BoxDecoration(
+              color: Colors.purpleAccent,
+              border: Border(
+                top: BorderSide(color: Colors.purpleAccent),
+                left: BorderSide(color: Colors.purpleAccent),
+                right: BorderSide(color: Colors.purpleAccent),
+                bottom: BorderSide(color: Colors.purpleAccent),
+              ),
+            ),
+            child: Text(
+              "Transfer",
+              style: TextStyle(fontSize: 17),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            margin: EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Image.network(
+                            'https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800.jpg'),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                      'Pique Bilang Wasit Untungkan \n Madrid, Koeman Tepok Jidat'),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
+            child: Text(
+              'Barcelona Feb 13, 2021',
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            margin: EdgeInsets.only(top: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Image.network(
+                            'https://images.daznservices.com/di/library/GOAL/c0/68/gerard-pique-barcelona-yellow-card-2020-21_1b5pixs2oir8s1hy0nwfy1tnrz.jpg?t=1931977831&quality=60&w=1200&h=800.jpg'),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                      'Pique Bilang Wasit Untungkan \n Madrid, Koeman Tepok Jidat'),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
+            child: Text(
+              'Barcelona Feb 13, 2021',
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
